02310e85bea3795cb8c832a134cdd3d3b3756e31
Pak <helicopterpak@gmail.com>
2023-03-01 20:33:30 +0300

    add receiver

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,8 @@
 package otus.gpb.homework.activities.receiver
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +10,21 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val posterImageView = findViewById<ImageView>(R.id.posterImageView)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val yearTextView = findViewById<TextView>(R.id.yearTextView)
+        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
+
+        intent.let {
+            val title = it.getStringExtra("title")
+            titleTextView.text = title.toString()
+            val year = it.getStringExtra("year")
+            yearTextView.text = year.toString()
+            val description = it.getStringExtra("description")
+            descriptionTextView.text = description.toString()
+            val image = getDrawable(R.drawable.niceguys)
+            posterImageView.setImageDrawable(image)
+        }
     }
 }
---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,42 @@
+package otus.gpb.homework.activities.sender
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sender)
+
+        val button: Button = findViewById<Button>(R.id.toGoogleMaps)
+        button.setOnClickListener {
+            val uri = Uri.parse("geo:0,0?q=рестораны")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage("com.google.android.apps.maps");
+            startActivity(intent)
+        }
+
+        val button2: Button = findViewById(R.id.sendEmail)
+        button2.setOnClickListener {
+            val uri = Uri.parse("mailto:" + "android@otus.ru" + "?subject=" + "классный курс!" + "&body=" + "спасибо всем причастным)))")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            startActivity(intent)
+        }
+
+        val button3: Button = findViewById(R.id.openReceiver)
+        button3.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                addCategory(Intent.CATEGORY_DEFAULT)
+                type = "text/plain"
+                putExtra("title", "Славные парни")
+                putExtra("year", "2016")
+                putExtra("description", "Что бывает, когда напарником брутального костолома становится субтильный лопух? Наемный охранник Джексон Хили и частный детектив Холланд Марч вынуждены работать в паре, чтобы распутать плевое дело о пропавшей девушке, которое оборачивается преступлением века. Смогут ли парни разгадать сложный ребус, если у каждого из них – свои, весьма индивидуальные методы.")
+            }
+            startActivity(intent)
+        }
+    }
+}
